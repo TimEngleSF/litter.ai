f72647bb5cd2fb204e8a6dce0116b07eae341172
Tim Engle <timothy.englew@gmail.com>
2024-09-18 22:02:46 -0700

    fix(middleware): ensure next is called in errorHandler

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,17 +1,21 @@
 const errorHandler = (error, req, res, next) => {
-    console.error('Error:', error);
+    if (error) {
+        console.error('Error:', error);
 
-    const statusCode = error.statusCode || 500;
+        const statusCode = error.statusCode || 500;
 
-    const message =
-        statusCode === 500 ? 'Internal Service Error' : error.message;
+        const message =
+            statusCode === 500 ? 'Internal Service Error' : error.message;
 
-    // Send a consistent error structure
-    return res.status(statusCode).json({
-        status: 'error',
-        message,
-        ...(process.env.NODE_ENV === 'dev' && { stack: error.stack }),
-    });
+        // Send a consistent error structure
+        return res.status(statusCode).json({
+            status: 'error',
+            message,
+            ...(process.env.NODE_ENV === 'dev' && { stack: error.stack }),
+        });
+    }
+
+    next();
 };
 
 export default errorHandler;
